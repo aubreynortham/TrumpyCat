3439247583a1228fe9a5ef63cafb2efa86bdba4e
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-09-12 11:36:39 -0400

    routes established

--- TrumpyCatReact/src/config/routes.js
+++ TrumpyCatReact/src/config/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Home from '../Home';
 import Main from '../Main';
+import Generate from '../Generate';
 
 var routes = (
   <Router history={hashHistory}>
