7e655ce3357edec37562c88797fd241eb53a8ff2
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-09-12 14:17:08 -0400

    sends buttons down to the bottom, adds randomize (both) button

--- TrumpyCatReact/src/Generate.js
+++ TrumpyCatReact/src/Generate.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import { Router, Link } from 'react-router';
 
 class Generate extends Component {
+  submitCatQuery(evt){
+    evt.preventDefault();
+  }
+  submitQuoteQuery(evt){
+    evt.preventDefault();
+  }
+  submitRandomQuery(evt){
+    evt.preventDefault();
+  }
   render() {
     return(
-      <div>
+      <div id="blackBar">
         <form id="inline">
-          <button id="catBtn"><i className="fa fa-refresh" aria-hidden="true"></i> Cat</button>
+          <button id="catBtn"><i className="fa fa-refresh" aria-hidden="true"></i> New Cat</button>
         </form>
 
         <form id="inline">
-          <button id="quoteBtn">Quote <i className="fa fa-refresh" aria-hidden="true"></i></button>
+          <button id="randomBtn">Rand<i className="fa fa-refresh" aria-hidden="true"></i>mize</button>
+        </form>
+
+        <form id="inline">
+          <button id="quoteBtn">New Quote <i className="fa fa-refresh" aria-hidden="true"></i></button>
         </form>
       </div>
 
