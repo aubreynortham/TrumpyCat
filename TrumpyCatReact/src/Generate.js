f315db45f4485c71f1a9de21c4eaeaa450271258
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-09-13 11:53:39 -0400

    styles applied to the content pulled in from APIs

--- TrumpyCatReact/src/Generate.js
+++ TrumpyCatReact/src/Generate.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Router, Link } from 'react-router';
 
 class Generate extends Component {
 
--- TrumpyCatReact/src/Results.js
+++ TrumpyCatReact/src/Results.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Router, Link } from 'react-router';
 import $ from 'jquery';
 
 class Results extends Component{
@@ -8,14 +9,19 @@ class Results extends Component{
       let content = $('<h1>').html(random.message).text();
       return (
         <div key={index}>
-          <h1>{content}</h1>
+          <Link to="/#">
+            <img id="cornerLogo" src="http://i.imgur.com/uPudds4.png" alt="Trumpy Cat!" />
+          </Link>
+          <h2><i className="fa fa-quote-left" aria-hidden="true"></i>
+            {content}
+           <i className="fa fa-quote-right" aria-hidden="true"></i></h2>
           <img className= "bigKitty" src={random.content} />
         </div>
-      )
-    })
+      );
+    });
     return (
       <div>{results}</div>
-    )
+    );
   }
 }
 
