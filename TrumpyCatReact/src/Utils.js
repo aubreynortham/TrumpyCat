5c5a6aa0f22c80973d745db5e41666fca832d91e
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-09-13 12:59:16 -0400

    replaces locally hosted cat API with deployed API url

--- TrumpyCatReact/src/Utils.js
+++ TrumpyCatReact/src/Utils.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 export function queryAPI (query) {
-  var catUrl = "http://localhost:8080/cats";
+  var catUrl = "http://trumpy-cat-api.herokuapp.com/cats";
   var quoteUrl = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
 
   return $.getJSON(quoteUrl).then(function(quoteResponse){ //first API query
