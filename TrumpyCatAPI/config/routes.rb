ee28174fe0ec9335160a74c800ff732a59a1ec85
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-09-13 14:47:47 -0400

    deletes locally hosted files for the rails cat API, has been moved to new repository

--- TrumpyCatAPI/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-
-  resources :cats, only: [:index]
-
-end
--- TrumpyCatAPI/db/migrate/20160912132235_create_cats.rb
+++
@@ -1,8 +0,0 @@
-class CreateCats < ActiveRecord::Migration[5.0]
-  def change
-    create_table :cats do |t|
-      t.string :content
-      t.timestamps
-    end
-  end
-end
