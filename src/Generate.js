df2888016549a35e398c6b19ce147511accec971
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-09-14 12:02:59 -0400

    moved pseudocode out of render

--- src/Generate.js
+++ src/Generate.js
@@ -4,17 +4,18 @@ class Generate extends Component {
 
   render() { //associates form submissions with functions that are defined in component's properties, from GenerateContainer.js
     let {handleSubmitCatQuery, handleSubmitQuoteQuery, handleSubmitRandomQuery} = this.props;
+
+    //button for new cat only
+    //button for new cat and new quote together
+    //button for new quote only
     return(
       <div id="blackBar">
-        //button for new cat only
         <form onSubmit={(evt) => handleSubmitCatQuery(evt)} id="inline">
           <button id="catBtn"><i className="fa fa-refresh" aria-hidden="true"></i> New Cat</button>
         </form>
-        //button for new cat and new quote together
         <form onSubmit={(evt) => handleSubmitRandomQuery(evt)}id="inline">
           <button id="randomBtn">Rand<i className="fa fa-refresh" aria-hidden="true"></i>mize</button>
         </form>
-        //button for new quote only
         <form onSubmit={(evt) => handleSubmitQuoteQuery(evt)}id="inline">
           <button id="quoteBtn">New Quote <i className="fa fa-refresh" aria-hidden="true"></i></button>
         </form>
--- src/Results.js
+++ src/Results.js
@@ -10,17 +10,17 @@ class Results extends Component{
     //check for html character entities in the seed data and convert to plain text
     trumpQuote = $('<h2>').html(trumpQuote.message).text();
 
+    //fixed corner logo links back to home page
+    //populate the random Trump quote within quote icons
+    //generate random cat pic for the 'background' image
     return (
       <div>
-        //fixed corner logo links back to home page
         <Link to="/TrumpyCat">
           <img id="cornerLogo" src="http://i.imgur.com/uPudds4.png" alt="Trumpy Cat!" />
         </Link>
-        //populate the random Trump quote within quote icons
         <h2><i className="fa fa-quote-left" aria-hidden="true"></i>
             {trumpQuote}
             <i className="fa fa-quote-right" aria-hidden="true"></i></h2>
-        //generate random cat pic for the 'background' image
         <img className= "bigKitty" src={catPic["content"]} />
       </div>
     );
